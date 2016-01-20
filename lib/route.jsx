8f8d5184f14b2f2b2d9dcb93b08a3465863128a0
Eero Leno <eero@leno.fi>
2016-01-20 15:23:28 +0200

    Added root route for responsivity

--- lib/route.jsx
+++ lib/route.jsx
@@ -6,3 +6,12 @@ FlowRouter.route('/path/:pathId', {
   },
   name: '<path>'
 });
+
+FlowRouter.route('/', {
+  action: function(params, queryParams) {
+    const containerElement = document.getElementById("render-target");
+    ReactDOM.render(<App _id={0} slug={null} />, containerElement);
+    console.log(params.pathId + " loaded with" + queryParams.slug);
+  },
+  name: '<root>'
+});
