1f19f40a7b2d3451ba2a0e7a6865e096e6983a1e
Eero Leno <eero@leno.fi>
2016-01-28 17:54:27 +0200

    add sidebar class

---
+++ Sidebar.jsx
@@ -0,0 +1,12 @@
+Sidebar = React.createClass({
+  render() {
+    return (
+      <div className="ui container">
+        <div className="ui button">göra saker</div>
+        <div className="ui button">nära dig</div>
+        <div className="ui input">
+          <input placeholder="Sök"></input>
+        </div>
+      </div>
+    );}
+});
