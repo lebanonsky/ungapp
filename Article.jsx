c4522937e57c10dcf1675f87fe87dc26ce4ee121
Kimmo <kimmo@netzionale.com>
2016-02-01 22:11:21 +0200

    add nice icons to data

--- Article.jsx
+++ Article.jsx
@@ -25,52 +25,52 @@ Article = React.createClass({
             <div className={divName} > 
             <div dangerouslySetInnerHTML={{__html: this.props.item.text }} />
 
-            <div class="ui list relaxed divided">
-              <div class="item">
-                <i class="map icon"></i>
-                <div class="content">
+            <div className="ui list relaxed divided">
+              <div className="item">
+                <i className="map icon"></i>
+                <div className="content">
                 {this.props.item.adress}
                 </div>
                </div> 
               
-              <div class="item">
-                <i class="wait icon"></i>
-                <div class="content">
+              <div className="item">
+                <i className="wait icon"></i>
+                <div className="content">
                 {this.props.item.tid}
                 </div>
               </div>
               
-              <div class="item">
-                <i class="phone icon"></i>
-                <div class="content">
+              <div className="item">
+                <i className="phone icon"></i>
+                <div className="content">
                 {this.props.item.tel}
                 </div>
               </div>
 
-              <div class="item">
-                <i class="mail icon"></i>
-                <div class="content">
+              <div className="item">
+                <i className="mail icon"></i>
+                <div className="content">
                 {this.props.item.epost}
                 </div>
               </div>                
 
-              <div class="item">
-                <i class="calendar icon"></i>
-                <div class="content">
+              <div className="item">
+                <i className="calendar icon"></i>
+                <div className="content">
                 {this.props.item.oppet}
                 </div>
             </div>
 
-              <div class="item">
-                <i class="linkify icon"></i>
-                <div class="content">
+              <div className="item">
+                <i className="linkify icon"></i>
+                <div className="content">
                 {this.props.item.webbsida}
                 </div>
             </div>
 
-              <div class="item">
-                <i class="bookmark icon"></i>
-                <div class="content">
+              <div className="item">
+                <i className="bookmark icon"></i>
+                <div className="content">
                 {this.props.item.link}
                 </div>
             </div>
