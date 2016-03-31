920d12d75559099b3cae03779e18563cdedb9d75
Teemu Ahonen <teemu.ahonen@web-veistamo.fi>
2016-03-31 11:58:30 +0300

    add iframe below header navigation

---
+++ ContentFrame.jsx
@@ -0,0 +1,11 @@
+ContentFrame = React.createClass({
+
+
+  render() {
+    return (
+
+          <div className="ui content segments ungapp">
+          <iframe src="http://dev.unginfo.fi/om-oss/" ></iframe>
+        </div>
+    );}
+});
