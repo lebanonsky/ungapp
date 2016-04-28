30150a883b7ed74c5bc1b1c86357371d369ebf20
Kimmo <kimmo@netzionale.com>
2016-04-28 10:01:51 +0300

    move frame up, align tillbaka

--- ContentFrame.jsx
+++ ContentFrame.jsx
@@ -10,31 +10,31 @@ ContentFrame = React.createClass({
 	  	if(this.props.slug == 'info') {
 		     return (<div>
 		     <div className="ui fixed menu white right">
-		     <i className="chevron left icon" onClick={this.closeFrame}> Tillbaka</i> 
+		     <i className="chevron left icon" onClick={this.closeFrame}> </i> <div>Tillbaka</div>
         </div>
         <iframe src="http://dev.unginfo.fi/om-oss/" ></iframe></div>)
 		} else if(this.props.slug == 'fraga') {
 			 return (<div>
 			 	<div className="ui fixed menu white right">
-		     <i className="chevron left icon" onClick={this.closeFrame}> Tillbaka</i> 
+		     <i className="chevron left icon" onClick={this.closeFrame}> </i> <div>Tillbaka</div>
         </div>
         <iframe src="http://fraga.luckan.fi/usp_form/fraga/" ></iframe></div>)
 		} else if(this.props.slug == 'chat') {
 			return (<div>	  		
 				<div className="ui fixed menu white right">
-       		     <i className="chevron left icon" onClick={this.closeFrame}> Tillbaka</i> 
+       		     <i className="chevron left icon" onClick={this.closeFrame}> </i> <div>Tillbaka</div>
         </div>
         <iframe src="http://svenska.yle.fi/sluta-panta" ></iframe></div>)
 		} else if(this.props.slug == 'direkt') {
 			return (<div>	  		
 				<div className="ui fixed menu white right">
-       		     <i className="chevron left icon" onClick={this.closeFrame}> Tillbaka</i> 
+       		     <i className="chevron left icon" onClick={this.closeFrame}> </i> <div>Tillbaka</div>
         		</div>
 	        <iframe src="http://unginfo.fi/ovriga-tjanster" ></iframe></div>)
 		} else {
 			return (
 			<div className="ui fixed menu white right">
-		     <i className="chevron left icon" onClick={this.closeFrame}> Tillbaka</i> 
+		     <i className="chevron left icon" onClick={this.closeFrame}> </i> <div>Tillbaka</div>
         </div>)
 		}
 	},
