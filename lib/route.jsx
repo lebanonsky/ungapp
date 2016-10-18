400e43fec37d995d457b78c76994f89dc8b35656
Kimmo <kimmo@netzionale.com>
2016-10-18 11:43:07 +0300

    show render-target when return

--- lib/route.jsx
+++ lib/route.jsx
@@ -80,13 +80,10 @@ FlowRouter.route('/', {
     const containerElement = document.getElementById("render-target");
     ReactDOM.render(<App _id={0} slug={null} />, containerElement);
     console.log(params.pathId + " loaded home " + queryParams.slug);
-    jQuery('#ungapp').removeClass();
-    jQuery('#ungapp').addClass('header pushable home');
+    $('#ungapp').removeClass();
+    $('#ungapp').addClass('header pushable home');
       }, false);
 
-
-
-  
 
   },
   name: '<root>'
